a4c9198c6cd8b0cac4f41d330ee569f24f801f90
krismos64 <134715062+krismos64@users.noreply.github.com>
2023-07-24 15:04:41 +0200

    intégration php validations

--- index.php
+++ index.php
@@ -1,3 +1,58 @@
+<?php
+$firstname = $name = $email = $phone = $message = "";
+$firstnameError = $nameError = $emailError = $phoneError = $messageError = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST")
+{
+  $firstname = verifyInput($_POST["firstname"]);
+  $name = verifyInput($_POST["name"]);
+  $email = verifyInput($_POST["email"]);
+  $phone = verifyInput($_POST["phone"]);
+  $message = verifyInput($_POST["message"]);
+
+
+if(empty($firstname))
+{
+  $firstnameError = "Donnez-nous votre prénom !";
+}
+if(empty($name))
+{
+  $nameError = "Donnez-nous votre nom !";
+}
+if(empty($message))
+{
+  $messageError = "Vous n'avez rien à nous dire ?";
+}
+if(!isEmail($email))
+{
+  $emailError = "Ce n'est pas une adresse mail valide !";
+}
+if(!isPhone($phone))
+{
+  $phoneError = "Ce n'est pas un numéro valide";
+}
+
+}
+
+function isPhone($var)
+{
+ return preg_match("/^[0-9 ]+$/",$var);
+}
+function isEmail($var)
+{
+  return filter_var($var, FILTER_VALIDATE_EMAIL);
+}
+
+function verifyInput($var)
+{
+  $var = trim($var);
+  $var = stripcslashes($var);
+  $var = htmlspecialchars($var);
+  return $var;
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="fr">
   <head>
@@ -25,64 +80,69 @@
       </div>
       <div class="row">
         <div class="col-lg-10 col-lg-offset-1">
-          <form id="contact-form" method="post" action="" role="form">
+          <form id="contact-form" method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" role="form">
             <div class="row">
               <div class="col-md-6">
                 <label for="firstname"
                   >Prénom <span class="blue">*</span></label
                 >
                 <input
-                  type="text"
+                  type="text" 
                   id="firstname"
                   name="firstname"
                   class="form-control"
                   placeholder="Votre prénom"
+                  value="<?php echo $firstname; ?>" 
                 />
-                <p class="comment">Message d'erreur</p>
+                <p class="comment"><?php echo $firstnameError; ?></p>
               </div>
               <div class="col-md-6">
                 <label for="name">Nom<span class="blue">*</span></label>
                 <input
-                  type="text"
+                  type="text" 
                   id="name"
                   name="name"
                   class="form-control"
                   placeholder="Votre nom"
+                  value="<?php echo $name; ?>"
                 />
-                <p class="comment">Message d'erreur</p>
+                <p class="comment"><?php echo $nameError; ?></p>
               </div>
               <div class="col-md-6">
                 <label for="email">Email <span class="blue">*</span></label>
                 <input
-                  type="text"
+                  type="email" 
                   id="email"
                   name="email"
                   class="form-control"
                   placeholder="Votre email"
+                  value="<?php echo $email; ?>"
                 />
-                <p class="comment">Message d'erreur</p>
+                <p class="comment"><?php echo $emailError; ?></p>
               </div>
               <div class="col-md-6">
                 <label for="phone">Téléphone <span class="blue">*</span></label>
                 <input
-                  type="text"
+                  type="tel" 
                   id="phone"
                   name="phone"
                   class="form-control"
                   placeholder="Votre n° de téléphone"
+                  value="<?php echo $phone; ?>"
                 />
-                <p class="comment">Message d'erreur</p>
+                <p class="comment"><?php echo $phoneError; ?></p>
               </div>
               <div class="col-md-12">
-                <label for="message">Message <span class="blue">*</span></label>
+                <label for="message">Message<span class="blue">*</span></label>
                 <textarea
                   id="message"
                   name="message"
                   class="form-control"
                   placeholder="Votre message"
-                  rows="4"
-                ></textarea>
-                <p class="comment">Message d'erreur</p>
+                  rows="4">
+                  <?php echo $message; ?>
+                </textarea>
+                <p class="comment"><?php echo $messageError; ?></p>
               </div>
               <div class="col-md-12">
                 <p class="blue">
